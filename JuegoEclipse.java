3b7aa3c345d665edaa08f385c0436d1ea18d085e
AntManFresc2006 <antoniomanuelfresco@gmail.com>
2024-11-29 11:55:21 +0100

    Ultima modificacion

--- JuegoEclipse.java
+++ JuegoEclipse.java
@@ -12,6 +12,7 @@ public class JuegoEclipse {
 		boolean certeza = false;
 		statsTotales = new int[6][4];
 		int[][] movimientos = new int[6][4];
+		int[][] puntosMovimientos = new int[6][5];
 
 		System.out.println("\n Saludos, este es un juego de batalla de hasta 5 personajes, que son los siguientes.");
 		System.out.println("\t1er personaje: Eclipse.");
@@ -151,7 +152,7 @@ public class JuegoEclipse {
 		}
 	}
 
-	public static boolean equalsParaGuillamon(String texto, String palabra) {
+	public static boolean equalsParaQueGuillamonNoMeMateUsandoElIgnoreCase(String texto, String palabra) {
 		Scanner sc = new Scanner(System.in);
 		boolean certeza;
 		String palabraTransformada = "";
@@ -176,15 +177,15 @@ public class JuegoEclipse {
 		int noSeComoLlamarEstaVariable = 0;
 		int ola = 0;
 
-		if (equalsParaGuillamon("atacar", opcion) == true) {
+		if (equalsParaQueGuillamonNoMeMateUsandoElIgnoreCase("atacar", opcion) == true) {
 
 			noSeComoLlamarEstaVariable = 1;
 			ola++;
-		} else if (equalsParaGuillamon("usarunobjeto", opcion) == true) {
+		} else if (equalsParaQueGuillamonNoMeMateUsandoElIgnoreCase("usarunobjeto", opcion) == true) {
 
 			noSeComoLlamarEstaVariable = 2;
 			ola++;
-		} else if (equalsParaGuillamon("tirarunamoneda", opcion) == true) {
+		} else if (equalsParaQueGuillamonNoMeMateUsandoElIgnoreCase("tirarunamoneda", opcion) == true) {
 
 			noSeComoLlamarEstaVariable = 3;
 			ola++;
@@ -194,13 +195,13 @@ public class JuegoEclipse {
 			System.out.println("\nError, tienes que escoger o 'Atacar', o 'Usar un objeto', o 'Tirar una moneda'.");
 			Scanner sc = new Scanner(System.in);
 			opcion = sc.nextLine();
-			if (equalsParaGuillamon("atacar", opcion) == true) {
+			if (equalsParaQueGuillamonNoMeMateUsandoElIgnoreCase("atacar", opcion) == true) {
 				ola++;
 				noSeComoLlamarEstaVariable = 1;
-			} else if (equalsParaGuillamon("usarunobjeto", opcion) == true) {
+			} else if (equalsParaQueGuillamonNoMeMateUsandoElIgnoreCase("usarunobjeto", opcion) == true) {
 				ola++;
 				noSeComoLlamarEstaVariable = 2;
-			} else if (equalsParaGuillamon("tirarunamoneda", opcion) == true) {
+			} else if (equalsParaQueGuillamonNoMeMateUsandoElIgnoreCase("tirarunamoneda", opcion) == true) {
 				ola++;
 				noSeComoLlamarEstaVariable = 3;
 			}
@@ -240,30 +241,41 @@ public class JuegoEclipse {
 	public static int[][] usabilidadMovs(int[][] informacion) {
 		// 0: HP, 1: Ataque, 2: Defensa, 3: Velocidad.
 		// 0: Eclipse, 1: Evil, 2: Cosmic, 3: Elina, 4: Keravnos, 5: ChuhZmR.
-		informacion[0][0] = 1750;
-		informacion[0][1] = 450;
-		informacion[0][2] = 400;
-		informacion[0][3] = 500;
-		informacion[1][0] = 1750;
-		informacion[1][1] = 500;
-		informacion[1][2] = 300;
-		informacion[1][3] = 450;
-		informacion[2][0] = 1750;
-		informacion[2][1] = 600;
-		informacion[2][2] = 250;
-		informacion[2][3] = 350;
-		informacion[3][0] = 1750;
-		informacion[3][1] = 400;
-		informacion[3][2] = 500;
-		informacion[3][3] = 550;
-		informacion[4][0] = 1750;
+		informacion[0][0] = 4;
+		informacion[0][1] = 10;
+		informacion[0][2] = 7;
+		informacion[0][3] = 1;
+		informacion[0][4] = 6;
+
+		informacion[1][0] = 5;
+		informacion[1][1] = 6;
+		informacion[1][2] = 7;
+		informacion[1][3] = 1; //Esta habilidad solo dura 3 turnos
+		informacion[1][4] = 3; // Esta solo dura 4 turnos
+
+		informacion[2][0] = 1; //Esta habilidad es infinita
+		informacion[2][1] = 3;
+		informacion[2][2] = 2; // esta dura 3 turnos y solo se puede usar despues de que el efecto de la anterior haya acabado
+		informacion[2][3] = 7;
+		informacion[2][4] = 5;
+
+		informacion[3][0] = 1; // Esta dura infinitamente
+		informacion[3][1] = 7;
+		informacion[3][2] = 5;
+		informacion[3][3] = 5;
+		informacion[3][4] = 6;
+
+		informacion[4][0] = 1;
 		informacion[4][1] = 400;
 		informacion[4][2] = 300;
 		informacion[4][3] = 700;
+		informacion[4][4] = 1;
+
 		informacion[5][0] = 1750;
 		informacion[5][1] = 400;
 		informacion[5][2] = 400;
 		informacion[5][3] = 400;
+		informacion[5][4] = 1;
 
 		return informacion;
 	}
@@ -275,22 +287,27 @@ public class JuegoEclipse {
 		informacion[0][1] = 450;
 		informacion[0][2] = 400;
 		informacion[0][3] = 500;
+
 		informacion[1][0] = 1750;
 		informacion[1][1] = 500;
 		informacion[1][2] = 300;
 		informacion[1][3] = 450;
+
 		informacion[2][0] = 1750;
 		informacion[2][1] = 600;
 		informacion[2][2] = 250;
 		informacion[2][3] = 350;
+
 		informacion[3][0] = 1750;
 		informacion[3][1] = 400;
 		informacion[3][2] = 500;
 		informacion[3][3] = 550;
+
 		informacion[4][0] = 1750;
 		informacion[4][1] = 400;
 		informacion[4][2] = 300;
 		informacion[4][3] = 700;
+
 		informacion[5][0] = 1750;
 		informacion[5][1] = 400;
 		informacion[5][2] = 400;
@@ -300,41 +317,47 @@ public class JuegoEclipse {
 	}
 
 	public static String[][] informacion(String[][] informacion) {
+
 		informacion[0][0] = "\nEclipse, este es el personaje principal y el héroe de este mundo, al igual que todos los personajes de este juego, sus habilidades son muy poderosas e interesantes. Procede de la raza 'Phaisa' y sus habilidades que lo caracterizan son la manipulación de su propia energia, la invulnerabilidad de ataques físicos y su Soulfuck.";
 		informacion[0][1] = "Clonación: Eclipse usará esta habilidad para crear una cantidad indefinida de clones, pueden ser 1 clon como mínimo, y 4 como máximo, solo durara 1 turno.";
-		informacion[0][2] = "Stat-Boosts: Eclipse potenciara un unico apartado de sus stats, el stat en cuestion sera completamente aleatorio y no se puede elegir.";
+		informacion[0][2] = "Stat-Boosts: Eclipse potenciará un único apartado de sus stats, el stat en cuestión sera completamente aleatorio y no se puede elegir.";
 		informacion[0][3] = "Soulfuck: este ataque consiste en una serie de movimientos que manipulan y atacan el alma del enemigo, y adquiere un bonus de poder de un x0.25 ante seres inmateriales como Cosmic o Evil.";
-		informacion[0][4] = "Adapting: Eclipse aprende el ultimo ataque o movimiento que haya usado su enemigo, sin incluir Divine-Attacks.";
+		informacion[0][4] = "Adapting: Eclipse aprende el último ataque o movimiento que haya usado su enemigo, sin incluir Divine-Attacks.";
 		informacion[0][5] = "Energy-Attacks: Eclipse lanza un orbe de energia contra el enemigo, existe una posibilidad del 20% de que ese ataque absorba la salud de su enemigo en favor del propio Eclipse.";
-		informacion[0][6] = "Intangibility: Los ataques fisicos tienen un 20% de probabilidad de fallar y potenciar el ataque y la defensa de Eclipse.";
-		informacion[1][0] = "\nEvil, este es el hermano gemelo de Eclipse, quien posee las mismas habilidades y hax que Eclipse, también es el rival malvado del mismo, asi que su fuerza de destrucción es mayor que la de Eclipse, pero Eclipse es mejor que Evil cuando se trata de inteligencia y durabilidad.";
+		informacion[0][6] = "Intangibility: Los ataques físicos tienen un 20% de probabilidad de fallar y potenciar el ataque y la defensa de Eclipse.";
+
+		informacion[1][0] = "\nEvil, este es el hermano gemelo de Eclipse, quién posee las mismas habilidades y hax que Eclipse, también es el rival malvado del mismo, asi que su fuerza de destrucción es mayor que la de Eclipse, pero Eclipse es mejor que Evil cuando se trata de inteligencia y durabilidad.";
 		informacion[1][1] = "Mental Manipulation: Evil lanzara pequeños orbes de energia que llegaran a la mente del enemigo y hara que se ataque a si mismo, este movimiento solo funciona con ataques no-fisicos, y siempre se realizara despues de que el enemigo lance su ataque.";
-		informacion[1][2] = "";
+		informacion[1][2] = "Energy-Storm: Evil invoca una cantidad muy grande de energía desde el cielo, esta energia cae al suelo con la intención de arrasar con el enemigo.";
 		informacion[1][3] = "Soulfuck: este ataque consiste en una serie de movimientos que manipulan y atacan el alma del enemigo, y adquiere un bonus de poder de un x0.25 ante seres inmateriales como Cosmic o Eclipse.";
-		informacion[1][4] = "";
-		informacion[1][5] = "Parasit: Evil lanzara una esfera de energia contra su enemigo, esta se impregnara dentro del cuerpo del enemigo y comenzara a chuparle la vida, y Evil se curara en base a la energia robada del usuario. La habilidad dura 4 turnos.";
+		informacion[1][4] = "Phaisa-Shield: Evil crea una armadura de energia que se impregna alrededor de su cuerpo, aumentando su defensa, y quitándole vida a cualquier enemigo que use ataques fisicos contra el.";
+		informacion[1][5] = "Parasit: Evil lanzará una esfera de energia contra su enemigo, esta se impregnará dentro del cuerpo del enemigo y comenzará a chuparle la vida, y Evil se curará en base a la energia robada del usuario. La habilidad dura 4 turnos.";
 		informacion[1][6] = "Intangibility: Los ataques fisicos tienen un 20% de probabilidad de fallar y potenciar el ataque y la defensa de Evil.";
+
 		informacion[2][0] = "\nCosmic, el dios del universo de Eclipse, sus habilidades llegan hasta niveles divinos, como la habilidad 'Time-Warping' y demás, que se explicarán en el panel de personajes.";
 		informacion[2][1] = "Body-Change: Cosmic cambiará de tipo, puede cambiar de tipo inmaterial a tipo fisico, y viceversa.";
-		informacion[2][2] = "Constraint: Cosmic usara sus poderes para inhabilitar uno de los movimientos de su enemigo por un periodo de 3 turnos, esta habilidad solo se podra usar nuevamente cuando acabe el efecto de la anterior.";
-		informacion[2][3] = "Cursed: Cosmic maldecira al enemigo, bajando sus probabilidades de acertar sus ataques al 40%, esta habilidad dura 3 turnos y solo se puede usar 2 veces.";
+		informacion[2][2] = "Constraint: Cosmic usará sus poderes para inhabilitar uno de los movimientos de su enemigo por un período de 3 turnos, esta habilidad solo se podrá usar nuevamente cuando acabe el efecto de la anterior.";
+		informacion[2][3] = "Cursed: Cosmic maldecirá al enemigo, bajando sus probabilidades de acertar sus ataques al 40%, esta habilidad dura 3 turnos y sólo se puede usar 2 veces.";
 		informacion[2][4] = "Witness-Oblivion: Cosmic ataca al enemigo con su espada divina.";
 		informacion[2][5] = "Divine-Flash: Cosmic invoca 2 orbes de energia que se fusionan entre sí y atacan al enemigo.";
 		informacion[2][6] = "Inmutable Body: Los ataques que intenten alterar los stats de Cosmic van a ser inútiles.";
-		informacion[3][0] = "\nElina, ella es otra de las heroinas de este mundo, su set de ataques consiste en la manipulacion del tiempo y la capacidad de atrapar al enemigo en dimensiones especiales, haciendo uso de las 'Cosmic-Skills', que le permite atacar fisicamente tanto a Eclipse como a Evil, atacar a dioses como Cosmic, y demas.";
-		informacion[3][1] = "Roots: Elina usara los recursos del medio ambiente y los transforma en energia vital de la cual ella se va a nutrir, curandole la vida por cada turno.";
+
+		informacion[3][0] = "\nElina, ella es otra de las heroínas de este mundo, es capaz de usar el medio ambiente a su favor, sus ataques constan del uso de elementos de la naturaleza, como los rayos, el hielo e incluso la energia vital de otros seres vivos.";
+		informacion[3][1] = "Roots: Elina usará los recursos del medio ambiente y los transforma en energia vital de la cuál ella se va a nutrir, curándole la vida por cada turno.";
 		informacion[3][2] = "Shell: Elina se protegera este turno, haciendo que cualquier ataque fisico o inmaterial del enemigo no surta efecto.";
 		informacion[3][3] = "Ray-Beam: Elina invocara un rayo que le hara daño, pero subira un 50% tanto su defensa como ataque.";
-		informacion[3][4] = "Frost-Attack: Elina endurece sus puños con guantes de temperaturas bajas, las temperaturas mas altas del guante son de -20 grados, y las minimas de -100 grados, a menor temperatura, mayor sera el daño.";
+		informacion[3][4] = "Frost-Attack: Elina endurece sus puños con guantes de temperaturas bajas y golpea con ellos al enemigo, las temperaturas mas altas del guante son de -20 grados, y las minimas de -100 grados, a menor temperatura, mayor sera el daño.";
 		informacion[3][5] = "Hurricane: Elina invoca un huracan que ataca a su enemigo, existe un 15% de probabilidad de bajar la defensa del enemigo.";
 		informacion[3][6] = "Armor: Elina posee una armadura que la cubre ante ataques fisicos y ataques mentales, su defensa sube un 10% ante ataques fisicos y un 25% ante ataques mentales.";
-		informacion[4][0] = "\nKeravnos, este es el mejor amigo de Eclipse, tiene habilidades de manipulacion del espacio-tiempo, Time-Stopping y demas.";
-		informacion[4][1] = "Item-Warping: Keravnos alterara la composicion fisica de todos los items del enemigo, haciendo que los elementos curativos le resten vida al enemigo, y que los elementos que potencien los atributos fisicos de su enemigo, se los baje.";
+
+		informacion[4][0] = "\nKeravnos, este es el mejor amigo de Eclipse, su set de ataques consiste en la manipulación del tiempo y la capacidad de atrapar al enemigo en dimensiones especiales, haciendo uso de las 'Cosmic-Skills', que le permite atacar físicamente tanto a Eclipse como a Evil, atacar a dioses como Cosmic, y demás.";
+		informacion[4][1] = "Item-Warping: Keravnos alterara la composición física de todos los ítems del enemigo, haciendo que los elementos curativos le resten vida al enemigo, y que los elementos que potencien los atributos fisicos de su enemigo, se los baje.";
 		informacion[4][2] = "Cosmic Skill: Black Holes: Keravnos enviara al enemigo a una dimension donde este no puede moverse ni defenderse, Keravnos lanzara una orda de agujeros negros, el minimo de agujeros negros es de 2 agujeros, y el maximo de 6.";
 		informacion[4][3] = "Cosmic Skill: Mental Illness: Keravnos enviara al enemigo a una dimension donde torturara mentalmente al enemigo mentalmente, quitándole vida y bajando ligeramente su defensa, este movimiento es mas destructivo contra seres como Cosmic, Eclipse o Evil, tambien existe una posbilidad del 15% de que el enemigo sufra trastornos cerebrales y pueda hacerse daño a si mismo.";
 		informacion[4][4] = "Time-Stop: Keravnos usara sus poderes sobrenaturales para detener el tiempo por el siguiente turno, esta tecnica solo se puede usar una sola vez.";
 		informacion[4][5] = "Fastering: Keravnos alterara el entorno de batalla, haciendo que los personajes mas rapidos sean los mas rapidos, esta habilidad dura 5 turnos en total.";
 		informacion[4][6] = "Equitative: los ataques fisicos que se lance contra seres inmateriales como Cosmic, Eclipse o Evil les hara daño, ignorando la intangibilidad de estos gracias a su Reality-Warping.";
+
 		informacion[5][0] = "\nChuhZmR, este es sin duda alguna el personaje mas desbalanceado del juego, tiende a ser perezoso y normalmente la suerte de este mismo es demasiado baja, por suerte, tiene habilidades como 'BeforeDay' o 'ClassRest' que lo convierten en alguien muy poderoso si lo sabes utilizar.";
 		informacion[5][1] = "BeforeDay: El usuario permanecera en estado de reposo por unos 3 turnos, al finalizar ese estado de reposo, este se recuperara toda su vida, y todos sus stats aumentaran x3.";
 		informacion[5][2] = "ClassRest: El usuario usará un turno para dormir, en este estado se recuperara el 50% de toda su vida.";
@@ -422,22 +445,22 @@ public class JuegoEclipse {
 	public static void mensajePersonaje(int personaje, String cadena) {
 		cadena = "Has escogido a... ";
 		if (personaje == 0) {
-			cadena += "Eclipse.";
+			cadena += "Eclipse, ¿A quién más piensas elegir?";
 		}
 		if (personaje == 1) {
-			cadena += "Evil.";
+			cadena += "Evil, ¿A quién más piensas elegir?";
 		}
 		if (personaje == 2) {
-			cadena += "Cosmic.";
+			cadena += "Cosmic, ¿A quién más piensas elegir?";
 		}
 		if (personaje == 3) {
-			cadena += "Elina.";
+			cadena += "Elina, ¿A quién más piensas elegir?";
 		}
 		if (personaje == 4) {
-			cadena += "Keravnos.";
+			cadena += "Keravnos, ¿A quién más piensas elegir?";
 		}
 		if (personaje == 5) {
-			cadena += "ChuhZmR.";
+			cadena += "ChuhZmR, ¿A quién más piensas elegir?";
 		}
 		System.out.println(cadena);
 	}
